342fcf1f24bd78e8c3c99f4e3348aa5d70f62c39
Felipe Fucci Rodrigues Lins <github.com.county380@passinbox.com>
2026-03-05 11:00:47 -0300

    added main code

---
+++ Gerador-de-Ticket-com-Painel.c
@@ -0,0 +1,137 @@
+/*
+Estrutura utilizada: Vetor circular de inteiros
+  - Inicio: indica qual índice o vetor começa.
+
+  - Fim: indica o índice que o vetor termina.
+
+  - Tamanho: indica quantos elementos o vetor possui no momento.
+
+Organização do código e separação de responsabilidades:
+
+  - inicializaHistorico: inicializa a estrutura do vetor circular.
+
+  - gerarTicket: gera um ticket, armazena ele no vetor circular e gerencia a
+    circularidade.
+
+  - formataTicket: formata o ticket no padrão correto e armazena o resultado no
+    vetor char temporário auxFormatacao.
+
+  - mostraHistorico: percorre o vetor circular, formata o ticket através da
+    função formataTicket, armazena no vetor temporário auxFormatacao, imprime o
+    resultado e o ciclo continua.
+
+  - main: inicialização do histórico, loop do painel e chamada de gerarTicket na
+    escolha 1 e chamada de mostrarHistorico na escolha 2.
+
+Observação 1: o vetor char temporário auxFormatacao foi utilizado para armazenar
+a string formatada temporariamente, isso permite que o vetor tickets fique
+intocado na hora da formatação e impressão. Dessa forma, podemos mudar a
+CAPACIDADE do vetor tickets sem afetar o restante do código.
+
+Observação 2: foi utilizado um TAMANHO_BUFFER maior do que o normal para
+permitir mudanças de formatos no futuro. Caso a nova formatação ultrapasse o
+TAMANHO_BUFFER, é necessário apenas aumentar o seu valor e alterar o sprintf da
+função formataTicket.
+
+*/
+
+#include <stdio.h>
+
+#define CAPACIDADE 5
+#define TAMANHO_BUFFER 32
+
+typedef struct {
+  int tickets[CAPACIDADE];
+  int inicio;
+  int fim;
+  int tamanho;
+
+} HistoricoTickets;
+
+void inicializaHistorico(HistoricoTickets *historico) {
+  historico->inicio = 0;
+  historico->fim = 0;
+  historico->tamanho = 0;
+}
+
+void gerarTicket(HistoricoTickets *historico, int novoTicket) {
+  historico->tickets[historico->fim] = novoTicket;
+  historico->fim = (historico->fim + 1) % CAPACIDADE;
+
+  if (historico->tamanho < CAPACIDADE) {
+    historico->tamanho++;
+  } else {
+    historico->inicio = (historico->inicio + 1) % CAPACIDADE;
+  }
+}
+
+void formataTicket(int numeroTicket, char *auxFormatacao) {
+  sprintf(auxFormatacao, "Q-%04d", numeroTicket);
+}
+
+void mostraHistorico(const HistoricoTickets *historico) {
+  if (historico->tamanho == 0) {
+    printf("Histórico de tickets vazio\n\n");
+    return;
+  }
+
+  char auxFormatacao[TAMANHO_BUFFER];
+  int posicao = historico->inicio;
+
+  for (int i = 0; i < historico->tamanho; i++) {
+    formataTicket(historico->tickets[posicao], auxFormatacao);
+    printf("%s | ", auxFormatacao);
+    posicao = (posicao + 1) % CAPACIDADE;
+  }
+  printf("\n\n");
+}
+
+int main() {
+
+  HistoricoTickets historico;
+  int opcao;
+  int resultado;
+  int valorTicket = 1;
+
+  inicializaHistorico(&historico);
+
+  do {
+    printf("\n----Sistema de Tickets----\n");
+    printf("Gerar novo ticket (digite 1)\n");
+    printf("Mostrar painel (digite 2)\n");
+    printf("Sair (digite 3)\n");
+    printf("\n\nEscolha: ");
+
+    resultado = scanf("%d", &opcao);
+    if (resultado != 1) {
+      while (getchar() != '\n')
+        ;
+      opcao = 0;
+    }
+
+    printf("\n\n");
+
+    switch (opcao) {
+
+    case 1:
+      gerarTicket(&historico, valorTicket);
+      printf("Ticket gerado!\n\n");
+      valorTicket++;
+      break;
+
+    case 2:
+      mostraHistorico(&historico);
+      break;
+
+    case 3:
+      printf("Saindo...\n\n");
+      break;
+
+    default:
+      printf("Opção inválida!\n\n");
+      break;
+    }
+  } while (opcao != 3);
+
+  return 0;
+}
